f927af8e6f6f714499b8b900f403ef3c7fd9a155
marmaestro <marmaestro@pm.me>
2026-01-06 20:16:13 +0100

    StudioCMS UI package update + theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 // noinspection SpellCheckingInspection
 
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
-import studiocmsUi from "@studiocms/ui";
+import studiocmsui from '@studiocms/ui';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	siteName: "marmaestro",
     site: "https://marmaestro.dev/",
 	prefetch: true,
+	favicon: "/wren-icon.png",
 	vite: {
 		ssr: {
-			noExternal: ["smartypants"],
+			noExternal: [ "smartypants" ],
 		},
-		plugins: [tailwindcss()]
+		plugins: [ tailwindcss() ]
 	},
-    integrations: [ icon(), studiocmsUi() ],
+    integrations: [ icon(), studiocmsui() ],
 });
