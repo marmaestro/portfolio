afdc3d90dd368321b809e3901c0f4f694aec99cd
marmaestro <marmaestro@pm.me>
2026-01-30 12:47:41 +0100

    i18n things? + pnpm update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,19 @@ import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import studiocmsUi from '@studiocms/ui';
+import {defaultLocale, locales} from "~/i18n/constants.ts";
 
 export default defineConfig({
 	siteName: "marmaestro",
 	site: "https://marmaestro.dev/",
 	prefetch: true,
+	i18n: {
+		locales: locales,
+		defaultLocale: defaultLocale,
+		routing: {
+			prefixDefaultLocale: true,
+		}
+	},
 	vite: {
 		plugins: [
 			tailwindcss({
