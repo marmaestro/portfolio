2480945547049cbd472e40696ba3deb619d7ea04
marmaestro <marmaestro@pm.me>
2026-04-10 14:58:54 +0200

    Web manifest automation

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,42 +10,61 @@ import studiocmsUi from '@studiocms/ui';
 import mdx from '@astrojs/mdx';
 import { fontProviders } from 'astro/config'
 
+import webmanifest from 'astro-webmanifest';
+
 export default defineConfig({
-	siteName: "marmaestro",
-	site: "https://marmaestro.dev",
+    siteName: "marmaestro",
+    site: "https://marmaestro.dev",
 
-	prefetch: true,
-	output: 'static',
-	trailingSlash: 'never',
-	build: {
-		assets: 'assets',
-		inlineStylesheets: 'always',
-		format: 'file'
-	},
+    prefetch: true,
+    output: 'static',
+    trailingSlash: 'never',
+    build: {
+        assets: 'assets',
+        inlineStylesheets: 'always',
+        format: 'file'
+    },
 
-	i18n: {
-		locales: ["en", "es"],
-		defaultLocale: "en",
-		routing: {
-			prefixDefaultLocale: false,
-		}
-	},
+    i18n: {
+        locales: ["en", "es"],
+        defaultLocale: "en",
+        routing: {
+            prefixDefaultLocale: false,
+        }
+    },
 
-	vite: {
-		plugins: [
-			tailwindcss({
-				applyBaseStyles: true
-			})
-		]
-	},
+    vite: {
+        plugins: [
+            tailwindcss({
+                applyBaseStyles: true
+            })
+        ]
+    },
 
-	integrations: [
+    integrations: [
 		icon(),
 		react(),
 		sitemap(),
 		studiocmsUi({
 			customCss: 'src/styles/overrides.css'
 		}),
-		mdx()
-	],
+		mdx(),
+		webmanifest({
+			name: 'marmaestro',
+			icon: 'src/assets/icon.png',
+			short_name: 'marmaestro',
+			description: "Mar Maestro Momparler's portfolio site. Game developer.",
+			start_url: '/',
+			lang: 'en',
+			display: 'standalone',
+
+			locales: {
+				es: {
+					name: 'marmaestro',
+					description: "Web portfolio de Mar Maestro Momparler. Desarrolladora de videojuegos.",
+					start_url: "/es",
+					lang: "es"
+				}
+			}
+		})],
 });
