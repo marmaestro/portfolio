be9ce60c495b40169ee32314ff9fa74503eeeaa9
marmaestro <marmaestro@pm.me>
2026-08-24 15:43:10 +0200

    CSS file migration to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,9 +35,7 @@ export default defineConfig({
 
     vite: {
         plugins: [
-            tailwindcss({
-                applyBaseStyles: true
-            })
+            tailwindcss()
         ]
     },
 
